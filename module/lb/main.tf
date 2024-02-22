b74f7ae3568df1a2550da2d6e2f04116382784ac
Shivampandhare <117571731+Shivampandhare@users.noreply.github.com>
2024-02-22 19:52:03 +0530

    Update main.tf

--- module/lb/main.tf
+++ module/lb/main.tf
@@ -18,7 +18,16 @@ resource "aws_lb_target_group" "tg" {
   }
 
 }
+resource "aws_lb_listener" "example_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
+  default_action {
+    target_group_arn = aws_lb_target_group.tg.arn
+    type             = "forward"
+  }
+}
 output "target_group_arns" {
     value   = aws_lb_target_group.tg.arn
 }
